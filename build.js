f76e23d4c468d339786afe74675f6c1d1daca450
Adnan Anjum <anjumcode12@gmail.com>
2026-05-26 21:35:58 +0500

    add refresh button

--- build.js
+++ build.js
@@ -1,22 +1,22 @@
+const container = document.querySelector(".container");
+const button = document.querySelector(".btn");
+
 const userName = async () => {
+  container.innerHTML = "";
   try {
     let response = await fetch("https://randomuser.me/api/?results=5");
     const { results } = await response.json();
-    // const { title, first, last } = results[0].name;
-    // console.log(`${title} ${first} ${last}`);
+
     cards(results);
   } catch (error) {
     console.log(error);
   }
 };
-
 userName();
 
+button.addEventListener("click", userName);
 const cards = (data) => {
-  let container = document.createElement("div");
   data.map((user) => {
-    console.log(user);
-    container.classList.add("container");
     let userCard = document.createElement("div");
     userCard.classList.add("user-card");
     let image = document.createElement("img");
@@ -28,12 +28,11 @@ const cards = (data) => {
     let age_details = document.createElement("div");
     age_details.textContent = `${user.dob.age} Yrs`;
     age_details.classList.add("age");
-
     userCard.appendChild(image);
     userCard.appendChild(user_name);
     userCard.appendChild(located);
     userCard.appendChild(age_details);
     container.appendChild(userCard);
-    document.body.appendChild(container);
+    return userCard;
   });
 };

fe32f93edcef2fb753acf9e5d16263a724110e49
Adnan Anjum <anjumcode12@gmail.com>
2026-05-26 22:09:21 +0500

    improved css

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ const button = document.querySelector(".btn");
 const userName = async () => {
   container.innerHTML = "";
   try {
-    let response = await fetch("https://randomuser.me/api/?results=5");
+    let response = await fetch("https://randomuser.me/api/?results=4");
     const { results } = await response.json();
 
     cards(results);
@@ -19,20 +19,24 @@ const cards = (data) => {
   data.map((user) => {
     let userCard = document.createElement("div");
     userCard.classList.add("user-card");
+
     let image = document.createElement("img");
     image.src = user.picture.medium;
+
     let user_name = document.createElement("h3");
     user_name.textContent = `${user.name.first} ${user.name.last}`;
+
     let located = document.createElement("p");
     located.textContent = user.location.city;
+
     let age_details = document.createElement("div");
     age_details.textContent = `${user.dob.age} Yrs`;
     age_details.classList.add("age");
+
     userCard.appendChild(image);
     userCard.appendChild(user_name);
     userCard.appendChild(located);
     userCard.appendChild(age_details);
     container.appendChild(userCard);
-    return userCard;
   });
 };
